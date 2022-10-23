d252393d6a4c99b4b2acb0b52665598b8d19a8ef
Tran Dai Quy <trandaiquy2903@gmail.com>
2022-10-23 11:01:46 +0700

    update(user - add to cart): successful notification

--- user.php
+++ user.php
@@ -36,7 +36,28 @@ if (empty($_SESSION['customer_id'])) {
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="notify.js"></script>
     <script type="text/javascript">
+        var getUrlParameter = function getUrlParameter(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+            return false;
+        };
+
         $(document).ready(function() {
+            let success = getUrlParameter('success');
+            if (success) {
+                $.notify(success, "success");
+            }
+
             $('.add-to-cart-btn').click(function() {
                 let id = $(this).data('id');
                 $.ajax({
